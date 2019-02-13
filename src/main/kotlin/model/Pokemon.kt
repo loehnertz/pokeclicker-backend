22d1c0d1fd034928c16712ecfcc72b155833e663
loehnertz <jakob2102@gmail.com>
2019-02-13 15:30:27 +0100

    Remove the default expression from the two models that were using SQL's 'DATETIME' type

--- src/main/kotlin/model/Pokemon.kt
+++ src/main/kotlin/model/Pokemon.kt
@@ -1,6 +1,5 @@
 package model
 
-import org.jetbrains.exposed.sql.CurrentDateTime
 import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 
@@ -9,5 +8,5 @@ object Pokemon : Table() {
     val pokeNumber = integer("pokeNumber")
     val owner = reference("owner", refColumn = User.id, onDelete = ReferenceOption.CASCADE)
     val xp = integer("xp")
-    val aquisitionDateTime = datetime("aquisitionDateTime").defaultExpression(CurrentDateTime())
+    val aquisitionDateTime = datetime("aquisitionDateTime")
 }
