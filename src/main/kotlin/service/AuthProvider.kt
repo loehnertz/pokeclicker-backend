717774b8b62d06c9c87eda51d7ba650385fadc67
Jakob Löhnertz <mail@jakob.codes>
2019-02-28 20:16:44 +0100

    Add a draft OAuth implementation

---
+++ src/main/kotlin/service/AuthProvider.kt
@@ -0,0 +1,15 @@
+package service
+
+import io.ktor.auth.OAuthServerSettings
+import io.ktor.http.HttpMethod
+
+val googleOauthProvider = OAuthServerSettings.OAuth2ServerSettings(
+    name = "google",
+    authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
+    accessTokenUrl = "https://www.googleapis.com/oauth2/v3/token",
+    requestMethod = HttpMethod.Post,
+
+    clientId = "xxxxxxxxxxx.apps.googleusercontent.com",
+    clientSecret = "yyyyyyyyyyy",
+    defaultScopes = listOf("profile") // no email, but gives full name, picture, and id
+)
