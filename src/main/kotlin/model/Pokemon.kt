cc7271774ac8d41d752de9cc7804a5198cdfcc93
Jakob Löhnertz <mail@jakob.codes>
2019-02-28 15:27:55 +0100

    Give the models data classes

--- src/main/kotlin/model/Pokemon.kt
+++ src/main/kotlin/model/Pokemon.kt
@@ -2,11 +2,20 @@ package model
 
 import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
+import java.time.LocalDateTime
 
-object Pokemon : Table() {
+object Pokemons : Table() {
     val id = integer("id").primaryKey().autoIncrement()
     val pokeNumber = integer("pokeNumber")
-    val owner = reference("owner", refColumn = User.id, onDelete = ReferenceOption.CASCADE)
+    val owner = reference("owner", refColumn = Users.id, onDelete = ReferenceOption.CASCADE)
     val xp = integer("xp")
     val aquisitionDateTime = datetime("aquisitionDateTime")
 }
+
+data class Pokemon(
+    val id: Int,
+    val pokeNumber: Int,
+    val owner: Users,
+    val xp: Int,
+    val aquisitionDateTime: LocalDateTime
+)
