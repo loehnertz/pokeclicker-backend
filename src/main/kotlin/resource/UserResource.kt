140cecd43872c8d104335dfd8b7a941537c80572
Jakob Löhnertz <mail@jakob.codes>
2019-03-06 18:04:40 +0100

    Add an endpoint for the user login

--- src/main/kotlin/resource/UserResource.kt
+++ src/main/kotlin/resource/UserResource.kt
@@ -1,27 +1,22 @@
 package resource
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import io.ktor.application.call
-import io.ktor.auth.OAuthAccessTokenResponse
-import io.ktor.auth.authenticate
-import io.ktor.auth.authentication
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
-import io.ktor.client.request.get
-import io.ktor.client.request.header
-import io.ktor.response.respondRedirect
+import io.ktor.request.receive
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.Route
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.route
 import io.ktor.sessions.get
 import io.ktor.sessions.sessions
-import io.ktor.sessions.set
 import main.Session
-import service.UserService
+import service.user.UserLoginRequest
+import service.user.UserRegistrationRequest
+import service.user.UserService
 
 fun Route.user(userService: UserService) {
     route("/users") {
@@ -29,18 +24,16 @@ fun Route.user(userService: UserService) {
             val session = call.sessions.get<Session>()
             call.respondText("Hello, ${session?.userId}")
         }
-        authenticate("google-oauth") {
-            route("/login") {
-                handle {
-                    val principal = call.authentication.principal<OAuthAccessTokenResponse.OAuth2>()
-                        ?: error("No principal")
 
-                    val json = HttpClient(Apache).get<String>("https://www.googleapis.com/userinfo/v2/me") {
-                        header("Authorization", "Bearer ${principal.accessToken}")
-                    }
-
-                    val data = ObjectMapper().readValue<Map<String, Any?>>(json)
-                    val id = data["id"] as String?
+        post("/login") {
+            try {
+                val loginRequest = call.receive<UserLoginRequest>()
+                val loginResponse = userService.loginUser(loginRequest)
+                call.respond(loginResponse)
+            } catch (exception: MissingKotlinParameterException) {
+                call.respond("You are missing one or multiple parameters")
+            }
+        }
 
                     if (id != null) call.sessions.set(Session(id))
                     call.respondRedirect("/")
