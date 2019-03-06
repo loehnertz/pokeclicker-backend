99a70e0e01453e93e359b99d10ec705739e93228
Jakob Löhnertz <mail@jakob.codes>
2019-03-06 18:44:28 +0100

    Remove the rest of the stubs from the initial user authentication

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,33 +3,22 @@ package main
 import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.Application
 import io.ktor.application.install
-import io.ktor.auth.Authentication
-import io.ktor.auth.oauth
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
+import io.ktor.features.CORS
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
-import io.ktor.features.origin
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
 import io.ktor.jackson.jackson
 import io.ktor.routing.Routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.sessions.SessionTransportTransformerMessageAuthentication
-import io.ktor.sessions.Sessions
-import io.ktor.sessions.cookie
-import io.ktor.util.hex
 import io.ktor.websocket.WebSockets
 import resource.store
 import resource.user
-import service.StoreService
-import service.UserService
 import service.store.StoreService
 import service.user.UserService
 import utility.DatabaseFactory
-import utility.googleOauthProvider
-
-class Session(val userId: String)
 
 fun Application.module() {
     install(DefaultHeaders)
