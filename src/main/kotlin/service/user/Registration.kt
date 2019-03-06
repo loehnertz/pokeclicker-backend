f4d47394f1471f36788f37c700ebf4e454242579
Jakob Löhnertz <mail@jakob.codes>
2019-03-06 20:04:04 +0100

    Shorten a method

--- src/main/kotlin/service/user/Registration.kt
+++ src/main/kotlin/service/user/Registration.kt
@@ -8,32 +8,36 @@ import org.mindrot.jbcrypt.BCrypt
 
 object Registration {
     fun registerUser(registrationRequest: UserRegistrationRequest): UserAuthenticationResponse {
-        if (userAlreadyExists(registrationRequest.username)) {
-            return UserAuthenticationResponse(error = "A user with your chosen name already exists")
+        return if (userAlreadyExists(registrationRequest.username)) {
+            UserAuthenticationResponse(error = "A user with your chosen name already exists")
         } else if (registrationRequest.password.length < 6) {
-            return UserAuthenticationResponse(error = "Password should be at least 6 characters long")
+            UserAuthenticationResponse(error = "Password should be at least 6 characters long")
         } else if (registrationRequest.username.length < 4) {
-            return UserAuthenticationResponse(error = "Username should be at least 4 characters long")
+            UserAuthenticationResponse(error = "Username should be at least 4 characters long")
         } else if (!usernameIsValid(registrationRequest.username)) {
-            return UserAuthenticationResponse(error = "Login should be consists of digits, letters, dots or underscores")
+            UserAuthenticationResponse(error = "Login should be consists of digits, letters, dots or underscores")
         } else {
-            val hashedPassword = BCrypt.hashpw(registrationRequest.password, BCrypt.gensalt())
+            createUserAccount(registrationRequest)
+        }
+    }
 
-            return try {
-                transaction {
-                    Users.insert {
-                        it[name] = registrationRequest.username
-                        it[email] = registrationRequest.email
-                        it[password] = hashedPassword
-                    }
+    private fun createUserAccount(registrationRequest: UserRegistrationRequest): UserAuthenticationResponse {
+        val hashedPassword = BCrypt.hashpw(registrationRequest.password, BCrypt.gensalt())
+
+        return try {
+            transaction {
+                Users.insert {
+                    it[name] = registrationRequest.username
+                    it[email] = registrationRequest.email
+                    it[password] = hashedPassword
                 }
-
-                val userToken = TokenManager.createToken(registrationRequest.username)
-
-                UserAuthenticationResponse(ok = true, token = userToken)
-            } catch (exception: Exception) {
-                UserAuthenticationResponse(error = "An unidentified error occurred during the account creation")
             }
+
+            val userToken = TokenManager.createToken(registrationRequest.username)
+
+            UserAuthenticationResponse(ok = true, token = userToken)
+        } catch (exception: Exception) {
+            UserAuthenticationResponse(error = "An unidentified error occurred during the account creation")
         }
     }
 
