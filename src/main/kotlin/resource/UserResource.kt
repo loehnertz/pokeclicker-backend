4e10f23d68ca177e0a4733ca0be04fa9d8c5a499
Jakob Löhnertz <mail@jakob.codes>
2019-03-06 22:17:52 +0100

    Add the second WebSocket endpoint for manual clicking in the front-end

--- src/main/kotlin/resource/UserResource.kt
+++ src/main/kotlin/resource/UserResource.kt
@@ -21,6 +21,13 @@ import service.user.TokenManager
 import service.user.UserLoginRequest
 import service.user.UserRegistrationRequest
 import service.user.UserService
+import java.util.concurrent.TimeUnit
+
+const val WebSocketClickingKeyword = "click"
+const val WebSocketClosingKeyword = "bye"
+const val WebSocketClickingMessage = "Click successfully received"
+const val WebSocketClosingMessage = "Connection closed by client"
+const val WebSocketUnknownCommandMessage = "The received command is unknown"
 
 fun Route.user(userService: UserService) {
     route("/users") {
@@ -51,7 +58,28 @@ fun Route.user(userService: UserService) {
             while (true) {
                 val currentBalance = balanceManager.retrieveCurrentBalance()
                 outgoing.send(Frame.Text(currentBalance.toString()))
-                delay(1000)
+                delay(TimeUnit.SECONDS.toMillis(1))
+            }
+        }
+
+        webSocket("/clicking") {
+            val user = TokenManager.verifyTokenAndRetrieveUser(call.parameters)
+            val balanceManager = userService.buildBalanceManager(user)
+
+            incoming.mapNotNull { it as? Frame.Text }.consumeEach { frame ->
+                val text = frame.readText()
+
+                when {
+                    text.equals(WebSocketClickingKeyword, ignoreCase = true) -> {
+                        balanceManager.increaseCurrentBalance()
+                        outgoing.send(Frame.Text(WebSocketClickingMessage))
+                    }
+                    text.equals(WebSocketClosingKeyword, ignoreCase = true) -> {
+                        outgoing.send(Frame.Text(WebSocketClosingMessage))
+                        close(CloseReason(CloseReason.Codes.NORMAL, message = WebSocketClosingMessage))
+                    }
+                    else -> outgoing.send(Frame.Text(WebSocketUnknownCommandMessage))
+                }
             }
         }
     }
