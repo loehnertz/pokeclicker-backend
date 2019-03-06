11762cac8e9911607f5fcb0642b52f35238f1681
Sander <29224798+SanderMeister@users.noreply.github.com>
2019-03-06 13:57:19 +0100

    basic user endpoint

--- src/main/kotlin/resource/UserResource.kt
+++ src/main/kotlin/resource/UserResource.kt
@@ -12,6 +12,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.request.get
 import io.ktor.client.request.header
+import io.ktor.response.respond
 import io.ktor.response.respondRedirect
 import io.ktor.response.respondText
 import io.ktor.routing.Route
@@ -21,6 +22,8 @@ import io.ktor.sessions.get
 import io.ktor.sessions.sessions
 import io.ktor.sessions.set
 import main.Session
+import model.Users
+import model.getUser
 import service.UserService
 
 fun Route.user(userService: UserService) {
@@ -29,6 +32,21 @@ fun Route.user(userService: UserService) {
             val session = call.sessions.get<Session>()
             call.respondText("Hello, ${session?.userId}")
         }
+
+        get("/{id}") {
+            val id = call.parameters["id"]
+            if (id != null) {
+                call.respond(Users.getUser(id.toInt()))
+            }
+        }
+
+        get("/{id}/pokemon") {
+            val id = call.parameters["id"]
+            if (id != null) {
+                call.respond(userService.getUserPokemon(id.toInt()))
+            }
+        }
+
         authenticate("google-oauth") {
             route("/login") {
                 handle {
--- src/main/kotlin/service/UserService.kt
+++ src/main/kotlin/service/UserService.kt
@@ -1,3 +1,19 @@
 package service
 
-class UserService
+import model.*
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+
+class UserService {
+
+    fun getUserPokemon(userId: Int): List<model.Pokemon> {
+
+        val ownedPokemons = transaction {
+            Pokemons.select{Pokemons.owner eq userId}.map { Pokemons.toPokemon(it) }
+        }
+
+        return ownedPokemons
+    }
+
+
+}
