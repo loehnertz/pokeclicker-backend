9ee3a0d6f7f74f083479bd5420c4950e8541da95
Jakob Löhnertz <mail@jakob.codes>
2019-03-06 22:30:15 +0100

    Fix the fact that the 'Scheduler' was blocking so far

--- src/main/kotlin/utility/Scheduler.kt
+++ src/main/kotlin/utility/Scheduler.kt
@@ -1,8 +1,8 @@
 package utility
 
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import service.user.BalanceManager
 import java.util.concurrent.TimeUnit
 
@@ -10,12 +10,10 @@ object Scheduler {
     private const val BalanceSyncTimeoutInMinutes: Long = 1
 
     fun scheduleRecurringTasks() {
-        runBlocking {
-            launch {
-                while (true) {
-                    BalanceManager.syncAllCurrentBalancesToDatabase()
-                    delay(TimeUnit.MINUTES.toMillis(BalanceSyncTimeoutInMinutes))
-                }
+        GlobalScope.launch {
+            while (true) {
+                BalanceManager.syncAllCurrentBalancesToDatabase()
+                delay(TimeUnit.MINUTES.toMillis(BalanceSyncTimeoutInMinutes))
             }
         }
     }
