5acc4566d094f72e475e003d406599fdec4ad1a5
Jakob Löhnertz <mail@jakob.codes>
2019-03-06 22:16:20 +0100

    Implement the first methods of the 'BalanceManager'

--- src/main/kotlin/service/user/BalanceManager.kt
+++ src/main/kotlin/service/user/BalanceManager.kt
@@ -1,12 +1,17 @@
 package service.user
 
 import model.User
+import model.Users
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
 import redis.clients.jedis.Jedis
 
 class BalanceManager(val user: User) {
-    private val redis = Jedis(System.getenv("redis_host"))
+    fun increaseCurrentBalance(increaseAmount: Long = 1): Long? {
+        return redis.hincrBy(RedisKeyUserBalances, user.name, increaseAmount)
+    }
 
-    fun retrieveCurrentBalance() {
-
+    fun retrieveCurrentBalance(): Long? {
+        return redis.hmget(RedisKeyUserBalances, user.name).firstOrNull()?.toLong()
     }
 }
