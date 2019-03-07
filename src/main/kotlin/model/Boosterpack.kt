d426658c1862ec5c846a98cd003dd83c8b628986
Jakob Löhnertz <mail@jakob.codes>
2019-03-07 19:16:38 +0100

    Incorporate the new 'ThinPokemon'

--- src/main/kotlin/model/Boosterpack.kt
+++ src/main/kotlin/model/Boosterpack.kt
@@ -2,9 +2,12 @@
 
 package model
 
+import service.store.data.ThinPokemon
+
 data class Boosterpack(
     val locationAreaId: Int,
     val name: String,
     val price: Long,
-    val hexColor: String
+    val hexColor: String,
+    val pokemons: List<ThinPokemon>
 )
