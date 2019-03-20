6056cb590e0d5e3c497f3442cedb717e8a14c3a2
Simon <simon.mailadres@gmail.com>
2019-03-20 06:36:53 +0100

    One in every 500 pokemon is a legendary

--- src/main/kotlin/service/store/StoreService.kt
+++ src/main/kotlin/service/store/StoreService.kt
@@ -25,7 +25,7 @@ const val RedisKeyBoosterpacks = "boosterpacks"
 const val RedisKeyBoosterpackIds = "boosterpack_ids"
 val LegendaryPokemon = intArrayOf(144, 145, 146, 150, 151, 243, 244, 245, 249, 250, 251, 377, 378, 379, 380, 381, 382, 383, 384, 385, 386, 480, 481, 482, 483, 484, 485, 486, 487, 488, 489, 490, 491, 492, 493, 494, 638, 639, 640, 641, 642, 643, 644, 645, 646, 647, 648, 649)
 val LegendaryPokemonXPMultiplier = BigDecimal(10)
-const val LegendaryPokemonInBoosterPack = 6 //Once every LegendaryPokemonInBoosterPack times there is a legendary pokemon in a booster pack. Please note that this doesn't mean that you'll also draw this pokemon.
+const val LegendaryPokemonInBoosterPack = 500 //Once every LegendaryPokemonInBoosterPack times there is a legendary pokemon in a booster pack. Please note that this doesn't mean that you'll also draw this pokemon.
 
 class StoreService {
     private val gson = Gson()
